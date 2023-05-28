37b920b805fd49d2a263d3304512b96a40cb4307
John Doe <antoine.naudy@gmail.com>
2023-05-28 15:59:03 +0200

    test g

--- tests/test_P7.py
+++ tests/test_P7.py
@@ -10,11 +10,16 @@ import mlflow
 import mlflow.sklearn
 import warnings
 
+# Ajoutez le chemin d'accès au répertoire 'app' contenant md.py
+#sys.path.append(os.path.abspath('..\\app'))
+
+#print(os.getcwd())
+  
 # Settings the warnings to be ignored
 warnings.filterwarnings('ignore')
 
 # Opening JSON file
-f = open('app\model\data.json')
+f = open('data.json')
   
 # returns JSON object as a dictionary
 data = json.load(f)
@@ -24,7 +29,7 @@ refused = [data['refused']]
 
 def test_predict_accepted():
   # Arrange
-  loaded_model = mlflow.sklearn.load_model("app/model")
+  loaded_model = mlflow.sklearn.load_model("model")
   print(loaded_model)
 
   # Act
@@ -34,39 +39,20 @@ def test_predict_accepted():
 
 def test_predict_refused():
   # Arrange
-  loaded_model = mlflow.sklearn.load_model("app/model")
+  loaded_model = mlflow.sklearn.load_model("model")
 
   # Act
   outcome = loaded_model.predict(refused)
   # Assert
   assert outcome == 1
+  '''
 
 def test_verif():
 
   # Act
-  outcome = os.getcwd()
+  outcome = 0
   # Assert
-  assert outcome == os.getcwd()
-
-'''if __name__ == "__main__" :
-  print(os.getcwd())
-  
-  # Settings the warnings to be ignored
-  warnings.filterwarnings('ignore')
-
-  # Opening JSON file
-  f = open("..\model\data.json")
-    
-  # returns JSON object as a dictionary
-  data = json.load(f)
-
-  accepted = [data['accepted']]
-  refused = [data['refused']]
-  
-  test_predict_accepted()
-  test_predict_refused()
-  test_verif()'''
-  
+  assert outcome == 0'''
 
 
 
